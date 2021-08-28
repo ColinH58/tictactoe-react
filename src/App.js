ba5725133676170f59afc42517cac9f9edc0a353
Colin Hoolwerf <choolwerf@gmail.com>
2021-08-28 11:49:47 -0500

    setup complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { useState } from 'react';
 import './index.css';
+import Board from './components/Board';
 
 function App() {
+
   return (
     <div className="App">
-      <h1>Tic Tac Toe Game</h1>
+        <h1 className="header">Tic Tac Toe!</h1>
+        <Board />
     </div>
   );
 }
---
+++ src/components/Square.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Square = () => {
+
+
+    return (
+        <div>
+            <button className="btn">X</button>
+        </div>
+    )
+}
+
+export default Square;
